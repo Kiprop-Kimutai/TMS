7089a7aa71d4adcc4f1ad373b232e0965d44ebca
jonah <jonah.kimutai@compulynx.com>
2018-12-05 13:54:45 +0300

    #core api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,18 @@
 var express = require('express');
+require('../models/db');
 var app = express();    
 var FirmwareRoutes = require('./firmware-routes');
 const DeviceRoutes = require('./device-routes');
-app.get('/',(requestAnimationFrame,res,next)=>{
+const TMSUploadFilesRoutes = require('./tms_upload_files-routes');
+const ZsequenceNumberRoutes = require('./zsequencenumbers-routes');
+const ResponseToPOSRoutes = require('./responsetoPos-routes');
+app.get('/',(req,res,next)=>{
     res.send("ok...");
 })
 app.use('/firmware_routes',FirmwareRoutes);
 app.use('/device_routes',DeviceRoutes);
+app.use('/tms_file_routes',TMSUploadFilesRoutes);
+app.use('/zsequence_routes',ZsequenceNumberRoutes);
+app.use('/scope_response_routes',ResponseToPOSRoutes);
 
 module.exports = app;
