445deb08dec82b6f8d76b6377d2adbf587ff59a2
jonah <jonah.kimutai@compulynx.com>
2018-12-05 14:38:47 +0300

    #manage on-session user addition to and removal from local storage

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable,of} from 'rxjs';
 import {delay,tap} from 'rxjs/operators';
-
+import {Users} from '../models/Users';
 @Injectable()
 export class AuthService{
   isLoggedIn:boolean = false;
@@ -10,12 +10,19 @@ export class AuthService{
     login(username:string,password:string):Observable<boolean>{
       console.log("Trying to login....");
       this.loggedInuser = username;
+      window.localStorage.setItem("userToken",JSON.stringify(user));
       //return Observable.of(true).pipe(delay(1000),tap(val=>isLoggedIn=true));
       return of(true).pipe(delay(100),tap(() => this.isLoggedIn =true));
     }
 
+    public getUserDetails(){
+      return localStorage.getItem("userToken");
+    }
     logout():void{
       this.isLoggedIn = false;
+      localStorage.removeItem("userToken");
     }
 
 }
+
+const user:Users = new Users(0,"alex","alexander","waiganjo","alexander@scope.wfp.org","");
