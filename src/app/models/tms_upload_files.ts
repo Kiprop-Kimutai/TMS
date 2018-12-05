b7e1bbc964624d156d6929b4036c2d70efb9317e
jonah <jonah.kimutai@compulynx.com>
2018-12-05 13:36:19 +0300

    #TypeScript model class for transaction batch

---
+++ src/app/models/tms_upload_files.ts
@@ -0,0 +1,34 @@
+export class TransactionFiles{
+    macAddress:string;
+    dateUploaded:string;
+    filename:string;
+    token:string;
+    flag:string;
+    valueTransactionCount:string;
+    valueTransactionAmount:string;
+    valueTransactionVoidCount:string;
+    valueTransactionVoidAmount:string;
+    commodityTransactionCount:String;
+    commodityTransactionAmount:string
+    commodityTransactionVoidCount:string;
+    commodityTransactionVoidAmount:string;
+    lastReceiptNo:string;
+
+    constructor(macAddress,dateUploaded,filename,token,flag,valueTransactionCount,valueTransactionAmount,valueTransactionVoidCount,
+        valueTransactionVoidAmount,commodityTransactionCount,commodityTransactionAmount,commodityTransactionVoidCount,commodityTransactionVoidAmount,lastReceiptNo){
+            this.macAddress=macAddress;
+            this.dateUploaded=dateUploaded;
+            this.filename =filename;
+            this.token = token;
+            this.flag=flag;
+            this.valueTransactionCount=valueTransactionCount;
+            this.valueTransactionAmount=valueTransactionAmount;
+            this.valueTransactionVoidCount=valueTransactionVoidCount;
+            this.valueTransactionVoidAmount=valueTransactionVoidAmount;
+            this.commodityTransactionCount=commodityTransactionCount;
+            this.commodityTransactionAmount=commodityTransactionAmount;
+            this.commodityTransactionVoidCount=commodityTransactionVoidCount;
+            this.commodityTransactionVoidAmount=commodityTransactionAmount;
+            this.lastReceiptNo=lastReceiptNo;
+        }
+}
