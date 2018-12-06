cb2daba3620320bf8bb7bbe2f2493e702685edb6
jonah <jonah.kimutai@compulynx.com>
2018-12-06 23:24:02 +0300

    #import FormsModule,ReactiveFormsModule and CommonModule to imports array

--- src/app/switch/switch.module.ts
+++ src/app/switch/switch.module.ts
@@ -2,14 +2,21 @@ import {NgModule} from '@angular/core';
 import {RouterModule,Routes,CanActivate} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import {TransactionsComponent} from './transactions/transactions.component';
+import {TransactionDetailComponent} from './transactions/transaction-details.component';
 import {ZsequenceComponent} from './zsequence/zsequence.component';
 import {ResponseComponent} from './response/response.component';
 import {SwitchService} from './switch.service';
 import {MaterialModule} from '../material.module';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 const routes:Routes = [
     {
         path:'transactions',
         component:TransactionsComponent
+    },
+    {
+        path:'transactions/:token',
+        component:TransactionDetailComponent
     },
     {
         path:'zsequence',
@@ -27,9 +34,9 @@ const routes:Routes = [
 ];
 
 @NgModule({
-    imports:[RouterModule.forChild(routes),MaterialModule],
+    imports:[RouterModule.forChild(routes),MaterialModule,FormsModule,ReactiveFormsModule,CommonModule],
     exports:[],
-    declarations:[TransactionsComponent,ZsequenceComponent,ResponseComponent],
+    declarations:[TransactionsComponent,TransactionDetailComponent,ZsequenceComponent,ResponseComponent],
     providers:[SwitchService]
 })
 export class SwitchModule{}
